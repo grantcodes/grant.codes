8358bb564688a69e6cd817cd3544a38febc98ed8
grantcodes <mail@grant.codes>
2020-07-16 01:18:43 +0200

    pages/feeds.js

--- components/Card/index.js
+++ components/Card/index.js
@@ -2,7 +2,7 @@ import classnames from 'classnames'
 
 export default ({ children, title, className }) => (
   <div className={classnames('card', className)}>
-    {!!title && <h2 class="card__title">{title}</h2>}
+    {!!title && <h2 className="card__title">{title}</h2>}
     {children}
   </div>
 )
