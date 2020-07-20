72d6f14ae6790ecf59792c985b88d90c01e4f5fa
grantcodes <mail@grant.codes>
2020-07-20 10:59:10 +0200

    reduce static generation count to 500

--- pages/[typeOrYear]/[month]/[day]/[slug].js
+++ pages/[typeOrYear]/[month]/[day]/[slug].js
@@ -21,13 +21,13 @@ const Page = ({ posts = [] }) => {
       }}
     />
   ) : (
-    posts.map((post, i) => <Post post={post} key={`post-single-${i}`} />)
-  )
+      posts.map((post, i) => <Post post={post} key={`post-single-${i}`} />)
+    )
 }
 
 export async function getStaticPaths() {
-  // Get a max of the 1000 last posts
-  const posts = await getPosts({ limit: 1000 }, true)
+  // Get a max of the 500 last posts
+  const posts = await getPosts({ limit: 500 }, true)
 
   // Helper function to add 0 to the start of numbers
   const leadingZero = (num) =>
