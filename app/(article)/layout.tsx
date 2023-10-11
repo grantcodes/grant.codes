a9222e2fddc8bdac46a302be6bb90014d813c6e2
Grant Richmond <mail@grant.codes>
2023-10-11 09:48:44 +0200

    tidy article layout component

--- app/(article)/layout.tsx
+++ app/(article)/layout.tsx
@@ -1,11 +1,11 @@
 import Container from 'components/Container'
 
-export default function DefaultPageLayout ({
-  // Layouts must accept a children prop.
-  // This will be populated with nested layouts or pages
-  children,
-}: {
+interface ArticleLayoutProps {
   children: React.ReactNode
-}) {
-  return <Container className='single-article'>{children}</Container>
 }
+
+const ArticleLayout = ({ children }: ArticleLayoutProps) => (
+  <Container className='single-article'>{children}</Container>
+)
+
+export default ArticleLayout
