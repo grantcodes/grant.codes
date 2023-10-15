ec80eb2820062039950106b89917b046bbd90a53
Grant Richmond <mail@grant.codes>
2023-10-15 16:07:37 +0200

    renable a couple of static params

--- app/(default)/page/[page]/page.tsx
+++ app/(default)/page/[page]/page.tsx
@@ -8,23 +8,30 @@ export interface PageParams {
 
 const Home = async ({ params }) => {
   const posts = await getPosts({ query: params })
-  return <PostList posts={posts} type='home' params={params} />
+  return (
+    <PostList
+      title={`Page ${params.page}`}
+      posts={posts}
+      type='home'
+      params={params}
+    />
+  )
 }
 
 // export const metadata = {
 //   title: 'Home', // TODO: Home page 2
 // }
 
-// export async function generateStaticParams(): Promise<PageParams[]> {
-//   // Count how many pages are needed
-//   const pageCount = await getPageCount()
-//   const pages: PageParams[] = []
-//   for (let pageNumber = 1; pageNumber < pageCount; pageNumber++) {
-//     pages.push({ page: `${pageNumber}` })
-//   }
+export async function generateStaticParams (): Promise<PageParams[]> {
+  // Count how many pages are needed
+  const pageCount = await getPageCount()
+  const pages: PageParams[] = []
+  for (let pageNumber = 1; pageNumber < pageCount; pageNumber++) {
+    pages.push({ page: `${pageNumber}` })
+  }
 
-//   return pages
-// }
+  return pages
+}
 
 export { generateMetadata } from 'lib/get/metadata'
 
--- app/(full-width)/photos/page/[page]/page.tsx
+++ app/(full-width)/photos/page/[page]/page.tsx
@@ -13,22 +13,22 @@ const Page = async ({ params }) => {
   )
 }
 
-// export async function generateStaticParams() {
-//   const params: any[] = []
-//   const ingoredTypes = ['photos', 'journals']
-//   // Get all types
-//   let types = await getTypes(true)
-//   types = types.filter((type) => !ingoredTypes.includes(type))
-//   for (const type of types) {
-//     // Get post count for each post type
-//     const pageCount = await getPageCount({ type })
-//     for (let pageNumber = 1; pageNumber < pageCount; pageNumber++) {
-//       params.push({ page: `${pageNumber}`, typeOrYear: type })
-//     }
-//   }
+export async function generateStaticParams () {
+  const params: any[] = []
+  const ingoredTypes = ['photos', 'journals']
+  // Get all types
+  let types = await getTypes(true)
+  types = types.filter(type => !ingoredTypes.includes(type))
+  for (const type of types) {
+    // Get post count for each post type
+    const pageCount = await getPageCount({ type })
+    for (let pageNumber = 1; pageNumber < pageCount; pageNumber++) {
+      params.push({ page: `${pageNumber}`, typeOrYear: type })
+    }
+  }
 
-//   // Return the paths, with a fallback to dynamically new / old categories
-//   return params
-// }
+  // Return the paths, with a fallback to dynamically new / old categories
+  return params
+}
 
 export default Page
