8d7a8a8fc234fc2f3baf490b3dc2df48770f730b
Grant Richmond <mail@grant.codes>
2023-10-15 15:56:15 +0200

    improve reveal.js loading

--- app/slides/Reveal.tsx
+++ app/slides/Reveal.tsx
@@ -1,10 +1,6 @@
 'use client'
 
 import { useEffect } from 'react'
-import RevealJs from 'reveal.js'
-import RevealMarkdown from 'reveal.js/plugin/markdown/markdown.esm'
-import RevealHighlight from 'reveal.js/plugin/highlight/highlight.esm'
-import RevealNotes from 'reveal.js/plugin/notes/notes.esm'
 
 import 'reveal.js/dist/reset.css'
 import 'reveal.js/dist/reveal.css'
@@ -12,17 +8,37 @@ import 'reveal.js/plugin/highlight/monokai.css'
 import 'reveal.js/dist/theme/simple.css'
 import './slides.scss'
 
+const importAll = async () => {
+  const RevealJs = await import('reveal.js')
+  const RevealMarkdown = await import('reveal.js/plugin/markdown/markdown.esm')
+  const RevealHighlight = await import(
+    'reveal.js/plugin/highlight/highlight.esm'
+  )
+  const RevealNotes = await import('reveal.js/plugin/notes/notes.esm')
+
+  return {
+    RevealJs: RevealJs.default,
+    RevealMarkdown: RevealMarkdown.default,
+    RevealHighlight: RevealHighlight.default,
+    RevealNotes: RevealNotes.default,
+  }
+}
+
 const Reveal = ({ children }: { children: React.ReactNode }) => {
   useEffect(() => {
-    let reveal = new RevealJs({
-      controls: false,
-      progress: true,
-      center: true,
-      transition: 'slide',
-      plugins: [RevealMarkdown, RevealHighlight, RevealNotes],
-    })
+    importAll().then(
+      ({ RevealJs, RevealNotes, RevealMarkdown, RevealHighlight }) => {
+        let reveal = new RevealJs({
+          controls: false,
+          progress: true,
+          center: true,
+          transition: 'slide',
+          plugins: [RevealMarkdown, RevealHighlight, RevealNotes],
+        })
 
-    reveal.initialize()
+        reveal.initialize()
+      }
+    )
   }, [])
 
   return (
