ec0b855d9ded9e88ae5b2cf618cfd842115c28f3
Grant Richmond <mail@grant.codes>
2024-09-13 10:53:58 +0200

    add gap to post list

--- components/PostList/Timeline.js
+++ components/PostList/Timeline.js
@@ -1,13 +1,16 @@
 import Post from '../Post'
 import Pagination from '../Container/Pagination'
+import * as styles from './Timeline.module.scss'
 
 const Timeline = ({ posts = [], title = '' }) => (
   <>
     {!!title && <h2 className="page-title">{title}</h2>}
 
-    {posts.map((post, i) => (
-      <Post post={post} key={`post-list-${i}`} compact />
-    ))}
+    <div className={styles.postList}>
+      {posts.map((post, i) => (
+        <Post post={post} key={`post-list-${i}`} compact />
+      ))}
+    </div>
 
     <Pagination noNext={posts.length < 10} />
   </>
