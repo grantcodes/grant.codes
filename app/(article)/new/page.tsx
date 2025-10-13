18e0731278447f24dff0aeed35d2d8519aad16a4
Grant Richmond <mail@grant.codes>
2025-10-13 10:28:04 +0200

    fix: issue on /new page

--- app/(article)/new/page.tsx
+++ app/(article)/new/page.tsx
@@ -1,37 +1,37 @@
-import Post from 'components/Post'
+import Post from "components/Post";
 
 const NewPostTemplate = () => (
-  <Post
-    className='postrchild-template'
-    post={{
-      type: ['h-entry'],
-      properties: {
-        name: ['New Post'],
-        content: [
-          {
-            html: [
-              '<p>This is my new post page. I use it with my postrchild extension to write new posts.</p>',
-            ],
-          },
-        ],
-        photo: [''],
-        url: [
-          typeof window !== 'undefined'
-            ? window.location.href
-            : process.env.NEXT_PUBLIC_URL,
-        ],
-      },
-      cms: { postType: 'article' },
-    }}
-  />
-)
+	<Post
+		className="postrchild-template"
+		post={{
+			type: ["h-entry"],
+			properties: {
+				name: ["New Post"],
+				content: [
+					{
+						html: [
+							"<p>This is my new post page. I use it with my postrchild extension to write new posts.</p>",
+						],
+					},
+				],
+				photo: [""],
+				url: [
+					typeof window !== "undefined"
+						? window.location.href
+						: process.env.NEXT_PUBLIC_URL?.replace(/['"]+/g, ""),
+				],
+			},
+			cms: { postType: "article" },
+		}}
+	/>
+);
 
-export default NewPostTemplate
+export default NewPostTemplate;
 
 export const metadata = {
-  title: 'New Post',
-  description: 'The new post page for grant.codes',
-  robots: {
-    index: false,
-  },
-}
+	title: "New Post",
+	description: "The new post page for grant.codes",
+	robots: {
+		index: false,
+	},
+};
